0224b1b917823a99318aaf2b49b8763b263e4e2f
Jason <Motl>
2021-09-29 13:53:33 -0500

    update2

--- js/app.js
+++ js/app.js
@@ -55,6 +55,10 @@ const app = new Vue({
                         'qui ad perditionem animarum pervagantur in mundo,\n' +
                         'Amen.',
                 },
+                {
+                    title: "Default Poem two",
+                    latin: 'Morbi mattis ullamcorper velit. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Fusce convallis metus id felis luctus adipiscing. Aenean massa. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nulla consequat massa quis enim. Praesent venenatis metus at tortor pulvinar varius. Donec venenatis vulputate lorem. Phasellus accumsan cursus velit. Pellentesque ut neque.',
+                }
             ],
 
 
